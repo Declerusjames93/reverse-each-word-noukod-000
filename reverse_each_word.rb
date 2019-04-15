22d406a7034df1ecbaa415bbad0f67fff33f7af9
James Declerus <james.declerus@esih.edu>
2019-04-15 19:16:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@
 def reverse_each_word(string)
   array = string.split(" ")
   our_array = []
-  array.each do|string|
+  array.collect do|string|
     our_array << string.reverse
   end
   our_array.join(" ")
